2cc470291a1801aef31f0485318fead2e9e630d5
danilgubarev <danilgubarev9804@gmail.com>
2025-01-24 13:24:37 +0200

    Nested Serializers, SerializerMethodField and Serializer Relations

--- DRFshop/api/serializers.py
+++ DRFshop/api/serializers.py
@@ -20,18 +20,29 @@ class ProductSerializer(serializers.ModelSerializer):
         return value
 
 class OrderItemSerializer(serializers.ModelSerializer):
+
+    product_name = serializers.CharField(source='product.name')
+    product_price = serializers.DecimalField(source='product.price', max_digits=10, decimal_places=2)
+
+
     class Meta:
         model = OrderItem
         fields = (
-                    'product',
+                    'product_name',
+                    'product_price',
                     'quantity',
+                    'item_subtotal'
                     
                 )
 
 class OrderSerializer(serializers.ModelSerializer):
 
     items = OrderItemSerializer(many=True, read_only=True)
-
+    total_price = serializers.SerializerMethodField(method_name='total')
+    def total(self, obj):
+        order_items = obj.items.all()
+        return sum(order_item.item_subtotal for order_item in order_items)
+    
     class Meta:
         model = Order
         fields = (
@@ -40,4 +51,5 @@ class OrderSerializer(serializers.ModelSerializer):
                     'user',
                     'status',
                     'items',
+                    'total_price'
                 )
