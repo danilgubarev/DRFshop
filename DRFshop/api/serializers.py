5042d0af5ec6fbf710e507d736d15a8949d8e6ba
danilgubarev <danilgubarev9804@gmail.com>
2025-01-24 12:33:02 +0200

    created models, api, serializers [api]

---
+++ DRFshop/api/serializers.py
@@ -0,0 +1,43 @@
+from rest_framework import serializers
+from .models import *
+
+class ProductSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Product
+        fields = (
+                    'id',
+                    'name',
+                    'description',
+                    'price',
+                    'stock',
+                )
+        
+    def validate_price(self, value):
+        if value <= 0:
+            raise serializers.ValidationError(
+                "Price must be greater than 0"
+            )
+        return value
+
+class OrderItemSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = OrderItem
+        fields = (
+                    'product',
+                    'quantity',
+                    
+                )
+
+class OrderSerializer(serializers.ModelSerializer):
+
+    items = OrderItemSerializer(many=True, read_only=True)
+
+    class Meta:
+        model = Order
+        fields = (
+                    'order_id',
+                    'created_at',
+                    'user',
+                    'status',
+                    'items',
+                )
