72535f1ebc0efa551f74e1a1fedfd9be58515b35
danilgubarev <danilgubarev9804@gmail.com>
2025-01-25 13:58:01 +0200

    crete new serializer for get info about product

--- DRFshop/api/views.py
+++ DRFshop/api/views.py
@@ -1,10 +1,11 @@
+from django.db.models import Max
 from django.http import JsonResponse
 from django.shortcuts import get_object_or_404
 
 from rest_framework.response import Response
 from rest_framework.decorators import api_view
 
-from api.serializers import ProductSerializer, OrderSerializer
+from api.serializers import ProductSerializer, OrderSerializer, ProductInfoSerializer
 from api.models import Product, Order
 
 @api_view(['GET'])
@@ -23,4 +24,14 @@ def product_detail(request, pk):
 def order_list(request):
     orders = Order.objects.all()
     serializer = OrderSerializer(orders, many=True)
+    return Response(serializer.data)
+
+@api_view(['GET'])
+def product_info(request):
+    products = Product.objects.all()
+    serializer = ProductInfoSerializer({
+        'products':products,
+        'count':len(products),
+        'max_price':products.aggregate(max_price = Max('price'))['max_price']
+    })
     return Response(serializer.data)
