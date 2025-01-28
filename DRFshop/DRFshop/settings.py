d22d1b64dc2b89eb251b78068cdc93c59f172b92
danilgubarev <danilgubarev9804@gmail.com>
2025-01-28 17:19:54 +0200

    use django-silk

--- DRFshop/DRFshop/settings.py
+++ DRFshop/DRFshop/settings.py
@@ -40,6 +40,7 @@ INSTALLED_APPS = [
 
     'django_extensions',
     'rest_framework',
+    'silk',
 
     'api',
 ]
@@ -52,6 +53,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'silk.middleware.SilkyMiddleware'
 ]
 
 ROOT_URLCONF = 'DRFshop.urls'
